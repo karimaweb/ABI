baf71eee9583b1a0cdd8621a02693743608a3904
valentin prost <valentinprost56@gmail.com>
2024-12-16 14:13:41 +0100

    nouveau script sql

--- DB/abi.sql
+++ DB/abi.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : ven. 13 déc. 2024 à 15:36
+-- Généré le : lun. 16 déc. 2024 à 13:12
 -- Version du serveur : 8.3.0
 -- Version de PHP : 8.2.18
 
@@ -29,20 +29,20 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `clients`;
 CREATE TABLE IF NOT EXISTS `clients` (
-  `IDCLIENT` int NOT NULL AUTO_INCREMENT,
-  `IDSECT` int NOT NULL,
-  `RAISONSOCIALE` char(50) NOT NULL,
-  `ADRESSECLIENT` char(32) DEFAULT NULL,
-  `CODEPOSTALCLIENT` char(5) DEFAULT NULL,
-  `VILLECLIENT` char(25) DEFAULT NULL,
+  `idClient` int NOT NULL AUTO_INCREMENT,
+  `raisonSociale` varchar(50) DEFAULT NULL,
+  `adresseClient` varchar(50) DEFAULT NULL,
+  `codePostal` varchar(50) DEFAULT NULL,
+  `villeClient` varchar(50) DEFAULT NULL,
   `CA` int DEFAULT NULL,
-  `EFFECTIF` int DEFAULT NULL,
-  `TELEPHONECLIENT` char(15) DEFAULT NULL,
-  `TYPECLIENT` char(10) NOT NULL,
-  `NATURECLIENT` char(15) NOT NULL,
-  `COMMENTAIRECLIENT` longtext,
-  PRIMARY KEY (`IDCLIENT`),
-  KEY `FK_AVOIRPOURSECTEUR` (`IDSECT`)
+  `effectifClient` int DEFAULT NULL,
+  `telephoneClient` varchar(50) DEFAULT NULL,
+  `typeClient` varchar(50) DEFAULT NULL,
+  `natureClient` varchar(50) DEFAULT NULL,
+  `commentaireClient` varchar(50) DEFAULT NULL,
+  `idSect` int NOT NULL,
+  PRIMARY KEY (`idClient`),
+  KEY `idSect` (`idSect`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -53,10 +53,10 @@ CREATE TABLE IF NOT EXISTS `clients` (
 
 DROP TABLE IF EXISTS `commander`;
 CREATE TABLE IF NOT EXISTS `commander` (
-  `IDCLIENT` int NOT NULL,
-  `CODEPROJET` int NOT NULL,
-  PRIMARY KEY (`IDCLIENT`,`CODEPROJET`),
-  KEY `FK_COMMANDER2` (`CODEPROJET`)
+  `codeProjet` int NOT NULL AUTO_INCREMENT,
+  `idClient` int NOT NULL,
+  PRIMARY KEY (`codeProjet`),
+  KEY `idClient` (`idClient`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -67,19 +67,18 @@ CREATE TABLE IF NOT EXISTS `commander` (
 
 DROP TABLE IF EXISTS `contacts`;
 CREATE TABLE IF NOT EXISTS `contacts` (
-  `IDCONTACT` int NOT NULL AUTO_INCREMENT,
-  `IDCLIENT` int NOT NULL,
-  `IDFONC` int NOT NULL,
-  `NOMCONTACT` char(32) NOT NULL,
-  `PRENOMCONTACT` char(32) NOT NULL,
-  `TELCONTACT` char(15) NOT NULL,
-  `EMAILCONTACT` char(50) DEFAULT NULL,
-  `PHOTO` char(255) DEFAULT NULL,
-  `DUREE` int DEFAULT NULL,
-  PRIMARY KEY (`IDCONTACT`),
-  KEY `FK_ASSOCIATION_5` (`IDFONC`),
-  KEY `FK_TRAVAILLERPOUR` (`IDCLIENT`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `idContact` int NOT NULL AUTO_INCREMENT,
+  `nomContact` varchar(50) DEFAULT NULL,
+  `prenomContact` varchar(50) DEFAULT NULL,
+  `telContact` varchar(50) DEFAULT NULL,
+  `emailContact` varchar(50) DEFAULT NULL,
+  `duree` int DEFAULT NULL,
+  `idFonc` int NOT NULL,
+  `idClient` int DEFAULT NULL,
+  PRIMARY KEY (`idContact`),
+  KEY `idFonc` (`idFonc`),
+  KEY `idClient` (`idClient`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -89,34 +88,30 @@ CREATE TABLE IF NOT EXISTS `contacts` (
 
 DROP TABLE IF EXISTS `documents`;
 CREATE TABLE IF NOT EXISTS `documents` (
-  `IDDOC` int NOT NULL AUTO_INCREMENT,
-  `IDCONTACT` int DEFAULT NULL,
-  `TITRE` char(255) NOT NULL,
-  `RESUME` char(255) DEFAULT NULL,
-  `DATEEDITION` date DEFAULT NULL,
-  PRIMARY KEY (`IDDOC`),
-  KEY `FK_PUBLIER` (`IDCONTACT`)
+  `idDoc` int NOT NULL AUTO_INCREMENT,
+  `titre` varchar(50) DEFAULT NULL,
+  `dateEdition` date DEFAULT NULL,
+  `resume` varchar(50) DEFAULT NULL,
+  `titreDoc` varchar(50) DEFAULT NULL,
+  `codeProjet` int NOT NULL,
+  `idContact` int DEFAULT NULL,
+  PRIMARY KEY (`idDoc`),
+  KEY `codeProjet` (`codeProjet`),
+  KEY `idContact` (`idContact`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `fonction`
+-- Structure de la table `fonctions`
 --
 
-DROP TABLE IF EXISTS `fonction`;
-CREATE TABLE IF NOT EXISTS `fonction` (
-  `IDFONC` int NOT NULL AUTO_INCREMENT,
-  `FONCTION` char(25) NOT NULL,
-  PRIMARY KEY (`IDFONC`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Déchargement des données de la table `fonction`
---
-
-INSERT INTO `fonction` (`IDFONC`, `FONCTION`) VALUES
-(1, 'test');
+DROP TABLE IF EXISTS `fonctions`;
+CREATE TABLE IF NOT EXISTS `fonctions` (
+  `idFonc` int NOT NULL AUTO_INCREMENT,
+  `fonction` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`idFonc`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -126,32 +121,37 @@ INSERT INTO `fonction` (`IDFONC`, `FONCTION`) VALUES
 
 DROP TABLE IF EXISTS `projets`;
 CREATE TABLE IF NOT EXISTS `projets` (
-  `CODEPROJET` int NOT NULL AUTO_INCREMENT,
-  `ABREGEPROJET` char(10) NOT NULL,
-  `NOMPROJET` char(32) NOT NULL,
-  `TYPEPROJET` char(25) NOT NULL,
-  PRIMARY KEY (`CODEPROJET`)
+  `codeProjet` int NOT NULL AUTO_INCREMENT,
+  `abrege` varchar(50) DEFAULT NULL,
+  `nomProjet` varchar(50) DEFAULT NULL,
+  `typeProjet` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`codeProjet`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `secteur_activite`
+-- Structure de la table `roles`
 --
 
-DROP TABLE IF EXISTS `secteur_activite`;
-CREATE TABLE IF NOT EXISTS `secteur_activite` (
-  `IDSECT` int NOT NULL AUTO_INCREMENT,
-  `ACTIVITE` char(25) NOT NULL,
-  PRIMARY KEY (`IDSECT`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+DROP TABLE IF EXISTS `roles`;
+CREATE TABLE IF NOT EXISTS `roles` (
+  `idRole` int NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`idRole`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
 
 --
--- Déchargement des données de la table `secteur_activite`
+-- Structure de la table `secteurs_activité`
 --
 
-INSERT INTO `secteur_activite` (`IDSECT`, `ACTIVITE`) VALUES
-(1, 'test');
+DROP TABLE IF EXISTS `secteurs_activité`;
+CREATE TABLE IF NOT EXISTS `secteurs_activité` (
+  `idSect` int NOT NULL,
+  `actitive` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`idSect`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -161,18 +161,50 @@ INSERT INTO `secteur_activite` (`IDSECT`, `ACTIVITE`) VALUES
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
-  `IDUSER` int NOT NULL AUTO_INCREMENT,
-  `LOGINUSER` char(50) NOT NULL,
-  `PASSUSER` char(15) NOT NULL,
-  PRIMARY KEY (`IDUSER`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `idUser` int NOT NULL AUTO_INCREMENT,
+  `loginUser` varchar(50) DEFAULT NULL,
+  `passUser` varchar(50) DEFAULT NULL,
+  `idRole` int NOT NULL,
+  PRIMARY KEY (`idUser`),
+  KEY `idRole` (`idRole`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Déchargement des données de la table `users`
+-- Contraintes pour les tables déchargées
 --
 
-INSERT INTO `users` (`IDUSER`, `LOGINUSER`, `PASSUSER`) VALUES
-(1, 'test', 'test');
+--
+-- Contraintes pour la table `clients`
+--
+ALTER TABLE `clients`
+  ADD CONSTRAINT `clients_ibfk_1` FOREIGN KEY (`idSect`) REFERENCES `secteurs_activité` (`idSect`);
+
+--
+-- Contraintes pour la table `commander`
+--
+ALTER TABLE `commander`
+  ADD CONSTRAINT `commander_ibfk_1` FOREIGN KEY (`codeProjet`) REFERENCES `projets` (`codeProjet`),
+  ADD CONSTRAINT `commander_ibfk_2` FOREIGN KEY (`idClient`) REFERENCES `clients` (`idClient`);
+
+--
+-- Contraintes pour la table `contacts`
+--
+ALTER TABLE `contacts`
+  ADD CONSTRAINT `contacts_ibfk_1` FOREIGN KEY (`idFonc`) REFERENCES `fonctions` (`idFonc`),
+  ADD CONSTRAINT `contacts_ibfk_2` FOREIGN KEY (`idClient`) REFERENCES `clients` (`idClient`);
+
+--
+-- Contraintes pour la table `documents`
+--
+ALTER TABLE `documents`
+  ADD CONSTRAINT `documents_ibfk_1` FOREIGN KEY (`codeProjet`) REFERENCES `projets` (`codeProjet`),
+  ADD CONSTRAINT `documents_ibfk_2` FOREIGN KEY (`idContact`) REFERENCES `contacts` (`idContact`);
+
+--
+-- Contraintes pour la table `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`idRole`) REFERENCES `roles` (`idRole`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
